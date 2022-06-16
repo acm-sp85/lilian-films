f0ec5620cc70e7900deab3f00c63b61781344442
Alex-new-laptop <acontell@gmail.com>
2022-06-15 20:20:45 -0400

    Added page info

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -7,6 +8,29 @@ export default function Home() {
         <a href="https://grainthemovie.com/">
           <img src="./lilian-logo.png" alt="logotype" className="logo"></img>
         </a>
+        <br />
+        <h3>Independent film production company based in Brooklyn, NY.</h3>
+        <div style={{ display: 'flexbox' }}>
+          <p> Founded in 2016 by </p>
+
+          <a
+            href="http://alexcontell.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Alex Contell
+          </a>
+
+          <p> and </p>
+
+          <a
+            href="https://www.tommasosacconi.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tommaso Sacconi.
+          </a>
+        </div>
       </div>
     </div>
   );
