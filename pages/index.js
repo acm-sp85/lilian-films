8a259423f0c44f584dac18c029720ad9765141a2
Alex-new-laptop <acontell@gmail.com>
2022-07-06 12:27:52 -0400

    scroll down option added

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,54 @@
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div>
-        <a href="https://grainthemovie.com/">
-          <img src="./lilian-logo.png" alt="logotype" className="logo"></img>
-        </a>
-        <br />
-        <h3>Independent film production company based in Brooklyn, NY.</h3>
-        <div style={{ display: 'flexbox' }}>
-          <p> Founded in 2016 by </p>
+    <div>
+      <div className={styles.container__1}>
+        <div className={styles.introCard}>
+          <img
+            src="./lilian-logo.png"
+            alt="logotype"
+            className={styles.introCard__logo}
+          />
 
-          <a
-            href="http://alexcontell.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Alex Contell
-          </a>
-
-          <p> and </p>
-
-          <a
-            href="https://www.tommasosacconi.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tommaso Sacconi.
-          </a>
+          <h3 className={styles.introCard__description}>
+            Independent film production company based in Brooklyn, NY.
+          </h3>
+          <h4 className={styles.introCard__leyend}>
+            Founded in 2016 by
+            <a
+              className={styles.introCard__link}
+              href="http://alexcontell.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Alex Contell
+            </a>
+            and
+            <a
+              className={styles.introCard__link}
+              href="https://www.tommasosacconi.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tommaso Sacconi.
+            </a>
+          </h4>
         </div>
+        <div className={styles.introCard__arrow}>V</div>
+      </div>
+      <div className={styles.container__2}>
+        <a
+          href="https://grainthemovie.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="./GRAIN_grainthemovie.png"
+            alt="grain-logo"
+            className={styles.container__2Logo}
+          />
+        </a>
       </div>
     </div>
   );
